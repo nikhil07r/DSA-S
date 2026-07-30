7b598a76a639e25d0a0857f7ad664c38715a5043
nikhil07r <nikhilrawat20062021@gmail.com>
2026-07-30 14:26:04 +0530

    Update

---
+++ technical skill dp/Equal.java
@@ -0,0 +1,13 @@
+// wap to find the largest of a & b . if a & b are equal then print "Both numbers are equal".
+public class Equal {
+    public static void main(String[] args) {
+        int a = 20, b = 20;
+        if (a > b) {
+            System.out.println("Largest: " + a);
+        } else if (b > a) {
+            System.out.println("Largest: " + b);
+        } else {
+            System.out.println("Both numbers are equal");
+        }
+    }
+}
