4b127959acc5e8d7d4ecdaca53d935d3885d5c9f
nikhil07r <nikhilrawat20062021@gmail.com>
2026-08-12 10:51:28 +0530

    Update

---
+++ technical skill/Armstrong.java
@@ -0,0 +1,17 @@
+// wap to print the Armstrong numbers.
+public class Armstrong {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            int sum = 0;
+            int temp = i;
+            while (temp != 0) {
+                int digit = temp % 10;
+                sum += digit * digit * digit;
+                temp /= 10;
+            }
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
+    }
+}
