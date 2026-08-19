488cca48b0106f5cf25b5f459fc76a61fd10f559
nikhil07r <nikhilrawat20062021@gamil.com>
2026-08-19 10:48:30 +0530

    Add prime checker function

---
+++ technical skill dp/PrimeChecker.java
@@ -0,0 +1,29 @@
+// wap to check if the given number is prime or not
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(n + " is prime");
+        else
+            System.out.println(n + " is not prime");
+    }
+}
