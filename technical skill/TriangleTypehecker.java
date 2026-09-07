be3ab7c71f05816921d8cef9e2fd43a5d5c851d1
nikhil07r <nikhilrawat20062021@gamil.com>
2026-09-07 15:31:07 +0530

    update

---
+++ technical skill/TriangleTypehecker.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TriangleTypehecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 3 sides: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a + b <= c || b + c <= a || a + c <= b) {
+            System.out.println("Invalid Triangle");
+        } else if (a == b && b == c) {
+            System.out.println("Equilateral");
+        } else if (a == b || b == c || a == c) {
+            System.out.println("Isosceles");
+        } else {
+            System.out.println("Scalene");
+        }
+    }
+}
